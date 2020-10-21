a57b1ef3cdb9252c57895005f14e5bad3237a52d
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-21 00:11:24 -0500

    add get posts method to ask for data

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose =require('mongoose');
+
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
+    type Post{
+        id: ID!
+        body: String!
+        createAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 const resolvers = {
      Query:{
-         sayHi: ()=> 'Hello World!'
+         async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch (err){
+                throw new Error(err);
+            }
+         }
      }
 }
 
@@ -17,7 +34,12 @@ const server = new ApolloServer({
     resolvers
 })
 
-server.listen({port: 5000})
+mongoose
+    .connect(MONGODB, {useNewUrlParser: true})
+    .then(()=> {
+        console.log('connected');
+        return server.listen({port: 5000});
+    })
     .then(res=> {
         console.log(`server running at ${res.url} `);
-    })
+    });
